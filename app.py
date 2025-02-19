629b16866ae6824b19b88edb564befcb046fb87b
Ahmad Raza <ahmadraza47@gmail.com>
2025-02-19 22:01:34 +0500

    Initial commit: Flask app setup for Vercel

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template_string("""
+        <!doctype html>
+        <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>Simple Flask App</title>
+        </head>
+        <body>
+            <h1>I am Ahmad Raza.</h1>
+            <p>VERCEL SCENES.</p>
+        </body>
+        </html>
+    """)
